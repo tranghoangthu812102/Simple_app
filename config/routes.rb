1b9dd408e5ffdfa859237d2e770b22d8ad5f6f78
Trang Hoang <tranghoang081299@gmail.com>
2021-08-08 16:23:38 +0700

    Chapter 8: Basic Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
     get "/home", to: "static_pages#home"
     get "/help", to: "static_pages#help"
     get "/contact", to: "static_pages#contact"
     get "/about", to: "static_pages#about"
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
     resources :users
   end
 end
